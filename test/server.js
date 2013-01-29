78574ed23d848017b863b3eec4dbc7a26df5e909
Tom Brow <tebrow@gmail.com>
2013-01-29 17:09:13 -0600

    Server and Client classes

--- test/server.js
+++ test/server.js
@@ -1,15 +1,33 @@
 var Server = require('../lib/server');
+var Client = require('../lib/client');
+var fs = require('fs');
+var assert = require('assert');
 
 describe('Server', function(){
 
-  var server = new Server();
+  it('reserves a slot that matches a goal', function(done){
+    var port = 8825,
+        goals = [{
+          host: 'Sonal Mane',
+          date: '2/7',
+          time: '1:00',
+          guest: 'Tom Brow',
+          email: 'tom@fiftyfourth.st',
+          topic: 'TBD' }],
+        email = fs.readFileSync('test/files/real_email.eml', 'utf8'),
+        client = new Client('localhost', port),
+        server = new Server(goals);
 
-  before(function(){
-    server.listen(8825);
-  })
-
-  after(function(done){
-    server.end(done);
-  })
+    server.listen(port);
+    client.send(
+      'slotgun@example.com',
+      'officehours@example.com',
+      email,
+      function(err){
+        assert.ifError(err);
+        server.end(done);
+      }
+    );
+  });
 
 })
