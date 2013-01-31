fe3f910fc274a65c6e28d5529dbef421cc817910
Tom Brow <tebrow@gmail.com>
2013-01-31 15:13:10 -0600

    functioning main.js

--- main.js
+++ main.js
@@ -1,4 +1,12 @@
 var Server = require('./lib/server');
 
-var server = new Server();
-server.listen(8025);
+var server = new Server([
+  { host: 'Tom Knight',
+    date: '1/17',
+    time: '2:55',
+    guest: 'Tom Brow',
+    email: 'tom@example.com',
+    topic: 'TBD' },
+]);
+
+server.listen(25);
